97a981473c66a186429db31ce4cb1c5794bedaca
linuskang <linus@kang.id.au>
2025-07-20 11:04:36 +1000

    moved: transfer to /devtesting for development testing,

---
+++ app/devtesting/loading/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Loading from "@/components/loading"
+
+export default function LoadingTestPage() {
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center">
+      <Loading />
+    </div>
+  )
+}
